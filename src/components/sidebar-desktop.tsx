1ead0286d72e824070599df9747473d724bcefaf
ju3tin <ju3tin@hotmail.co.uk>
2024-11-03 23:39:13 +0000

    wer

--- src/components/sidebar-desktop.tsx
+++ src/components/sidebar-desktop.tsx
@@ -21,7 +21,7 @@ export function SidebarDesktop(props: SidebarDesktopProps) {
     <aside className='w-[270px] max-w-xs h-screen fixed left-0 top-0 z-40 border-r'>
       <div className='h-full px-3 py-4'>
         <h3 className='mx-3 text-lg font-semibold text-foreground'>
-          <Image alt='' width={100} height={100}  src="/image07.png" />
+        <Image alt='' width={300} height={300} src="/image07.png" />
         </h3>
         <div className='mt-5'>
           <div className='flex flex-col gap-1 w-full'>
