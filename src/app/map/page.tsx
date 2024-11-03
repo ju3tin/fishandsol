da3014027e08e201807ec96b31df7faae9e519f6
ju3tin <ju3tin@hotmail.co.uk>
2024-11-03 15:11:21 +0000

    wer

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,19 +1,21 @@
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
 
-export default async function Page() {
-    const Map = useMemo(() => dynamic(
-        () => import('@/components/map/'),
-        {
-            loading: () => <p>A map is loading</p>,
-            ssr: false
-        }
-    ), [])
+const Map = dynamic(
+    () => import('@/components/map/'),
+    {
+        loading: () => <p>A map is loading</p>,
+        ssr: false
+    }
+);
+
+export default function Page() {
+    const MemoizedMap = useMemo(() => <Map posix={[51.5074, -0.1278]} />, []);
 
     return (
         <>
             <div className="bg-white-700 mx-auto my-5 w-[98%] h-[480px]">
-                <Map posix={[51.5074, -0.1278]} />
+                {MemoizedMap}
             </div>
         </>
     )
