b63447f632581eabb7038cc574756abdc8943ba8
ju3tin <ju3tin@hotmail.co.uk>
2024-11-04 13:53:58 +0000

    werwer

---
+++ src/app/social/page.tsx
@@ -0,0 +1,10 @@
+
+export default function social() {
+    return (
+        <div>
+<blockquote className="twitter-tweet" data-dnt="true"><p lang="en" dir="ltr">It&#39;s pretty awesome how dancing makes robots less intimidating. Looking forward to seeing more nontrivial Machine Learning on these robots. Credit: Boston Dynamics. <a href="https://t.co/wnB2i9qhdQ">pic.twitter.com/wnB2i9qhdQ</a></p>&mdash; Reza Zadeh (@Reza_Zadeh) <a href="https://twitter.com/Reza_Zadeh/status/1344009123004747778?ref_src=twsrc%5Etfw">December 29, 2020</a></blockquote>
+<script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+</div>
+)}
+
+
