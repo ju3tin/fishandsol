ec23c1084a27b8fa141a1e5bab0d920b80289eac
ju3tin <ju3tin@hotmail.co.uk>
2024-11-04 15:01:18 +0000

    werwer

---
+++ src/app/components/TwitterEmbed.js
@@ -0,0 +1,25 @@
+// components/TwitterEmbed.js
+import React, { useEffect } from 'react';
+
+const TwitterEmbed = ({ tweetUrl }) => {
+  useEffect(() => {
+    // Load the Twitter widgets.js script
+    const script = document.createElement('script');
+    script.src = 'https://platform.twitter.com/widgets.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Clean up the script when the component is unmounted
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <blockquote className="twitter-tweet">
+      <a href={tweetUrl}>Check out this tweet!</a>
+    </blockquote>
+  );
+};
+
+export default TwitterEmbed;
--- src/app/social/page.tsx
+++ src/app/social/page.tsx
@@ -2,15 +2,20 @@
 import React from 'react';
 import InstagramEmbed from '../components/InstagramEmbed';
 import TikTokEmbed from '../components/TikTokEmbed';
+import TwitterEmbed from '../components/TwitterEmbed';
 
 
 export default function social() {
+    const tweetUrl = 'https://twitter.com/ju3t1ng/status/1845047137077203376';
     const tiktokPostUrl = 'https://www.tiktok.com/@ju3ting/video/7314258976999361824';
     const instagramPostUrl = 'https://www.instagram.com/p/DB8SQnFuLdS/';
     return (
         <div>
-<blockquote className="twitter-tweet" data-dnt="true"><p lang="en" dir="ltr">It&#39;s pretty awesome how dancing makes robots less intimidating. Looking forward to seeing more nontrivial Machine Learning on these robots. Credit: Boston Dynamics. <a href="https://t.co/wnB2i9qhdQ">pic.twitter.com/wnB2i9qhdQ</a></p>&mdash; Reza Zadeh (@Reza_Zadeh) <a href="https://twitter.com/Reza_Zadeh/status/1344009123004747778?ref_src=twsrc%5Etfw">December 29, 2020</a></blockquote>
-<script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+            
+            <div>
+            <br></br>
+      <TwitterEmbed tweetUrl={tweetUrl} />
+    </div>
 
 <div>
      <br></br>
@@ -20,7 +25,7 @@ export default function social() {
     <div>
     <br></br>
       <TikTokEmbed url={tiktokPostUrl} />
-      <script async src="https://www.tiktok.com/embed.js"></script> {/* Add this line to load TikTok's embed script */}
+      <script async src="https://www.tiktok.com/embed.js"></script>
     </div>
 
 
