5de1341c935a21811e594b8c03976d1e2ef1817c
ju3tin <ju3tin@hotmail.co.uk>
2024-11-04 12:24:20 +0000

    werwer

---
+++ src/app/game/page.tsx
@@ -0,0 +1,14 @@
+// pages/index.js
+export default function Home() {
+    return (
+      <div>
+        <iframe
+          src="/index1.html"
+          width="100%"
+          height="600"
+          style={{ border: '1px solid black' }}
+          title="Example Iframe"
+        ></iframe>
+      </div>
+    );
+  }
