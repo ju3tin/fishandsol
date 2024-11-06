74e084e17156455a76529327e84785edbea8e77d
ju3tin <ju3tin@hotmail.co.uk>
2024-11-06 19:14:21 +0000

    asd

--- src/app/map3/page.js
+++ src/app/map3/page.js
@@ -9,6 +9,31 @@ import '../../../public/css/leaflet.draw.css'
 
 const center = [52.22977, 21.01178];
 
+
+const points = [
+  {
+    lat: 52.230020586193795,
+    lng: 21.01083755493164,
+    title: 'point 1'
+  },
+  {
+    lat: 52.22924516170657,
+    lng: 21.011320352554325,
+    title: 'point 2'
+  },
+  {
+    lat: 52.229511304688444,
+    lng: 21.01270973682404,
+    title: 'point 3'
+  },
+  {
+    lat: 52.23040500771883,
+    lng: 21.012146472930908,
+    title: 'point 4'
+  },
+];
+
+
 // -------------------------------------------
 
 // The best way to add the following code to your style
