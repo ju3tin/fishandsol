5fb539efcad8afd1f631618541b239f83690bb90
ju3tin <ju3tin@hotmail.co.uk>
2024-11-06 17:59:38 +0000

    asd

---
+++ src/app/roadmap/page.tsx
@@ -0,0 +1,26 @@
+
+export default function Roadmap() {
+    return (
+<div id="container12" className="container default">
+    <div className="wrapper">
+        <div className="inner">
+            <h2 id="text15" className="style3" style={{opacity: 1, transform: 'none'}}>
+                <span className="p">0. Launch token - DONE</span><br />
+                <span className="p">1. Reach $10 Million Marketcap - done</span><br />
+                <span className="p">2. build community - in progress</span><br />
+                <span className="p">3. Big marketing - in progress</span><br />
+                <span className="p">4. vibe - in progress</span><br />
+                <span className="p">5. major marketing - in progress</span><br />
+                <span className="p">6. major Listings - tba</span><br />
+                <span className="p">7. claim #1 spot of biggest Pump.fun project EVER - TBA</span><br />
+                <span className="p">8. vibe more - TBA</span><br />
+                <span className="p">9. More marketing - TBA</span><br />
+                <span className="p">10. More listing - tba</span><br />
+                <span className="p">11. MAJOR VIBES - TBA</span><br />
+                <span className="p">12. Reach $1 Billion+ in Marketcap - in progress</span><br />
+                <span className="p">13. Keep building.</span><br />
+            </h2>
+        </div>
+    </div>
+</div>
+)}
