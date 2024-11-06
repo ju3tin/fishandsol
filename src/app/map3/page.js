4906c227fd630e9d03668144dd2c76eece47a96c
ju3tin <ju3tin@hotmail.co.uk>
2024-11-06 19:12:49 +0000

    asd

--- src/app/map3/page.js
+++ src/app/map3/page.js
@@ -1,6 +1,6 @@
 "use client"
 import { useRef, useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import tileLayer from "../../../util/tileLayer";
 import '../../../public/leaflet.css'; // Ensure Leaflet CSS is imported
 import '../../../public/css/leaflet.draw.css'
@@ -19,54 +19,17 @@ const center = [52.22977, 21.01178];
 
 // Or -----------------------------------------
 
-
-const points = [
-  {
-    lat: 52.230020586193795,
-    lng: 21.01083755493164,
-    title: 'point 1'
-  },
-  {
-    lat: 52.22924516170657,
-    lng: 21.011320352554325,
-    title: 'point 2'
-  },
-  {
-    lat: 52.229511304688444,
-    lng: 21.01270973682404,
-    title: 'point 3'
-  },
-  {
-    lat: 52.23040500771883,
-    lng: 21.012146472930908,
-    title: 'point 4'
-  },
-];
-
-
-const MyMarkers = ({ data }) => {
-  return data.map(({ lat, lng, title }, index) => (
-    <Marker
-      key={index}
-      position={{ lat, lng }}
-    >
-      <Popup>{title}</Popup>
-    </Marker>
-  ));
-}
-
 const MapWrapper = () => {
   const [map, setMap] = useState(null);
   const tileRef = useRef(null);
 
   useEffect(() => {
+    
     if (!map) return;
     tileRef.current.getContainer().style.setProperty("filter", `grayscale(1)`);
   }, [map]);
-
-
-
-  
+  if (typeof window !== 'undefined') {
+            
   return (
     <div>
     <MapContainer
@@ -77,11 +40,9 @@ const MapWrapper = () => {
       style={{ height: '100vh', width: '100%' }}
     >
       <TileLayer ref={tileRef} {...tileLayer} />
-      <MyMarkers data={points} />
-
     </MapContainer>
     </div>
   );
-};
+}};
 
 export default MapWrapper;
