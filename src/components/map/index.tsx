394c381d0e2370c73d59bc7dfddaf87d8937c736
Justin <ju3tin@hotmail.co.uk>
2024-11-06 00:09:29 +0000

    Update index.tsx

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -30,7 +30,7 @@ const Map = (Map: MapProps) => {
             style={{ height: "100%", width: "100%" }}
         >
             <TileLayer
-                url="http://{S}.tiles.mapbox.com/v3/stamen.trees-cabs-crime/{Z}/{X}/{Y}.png"
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
                {markerData.map((marker, index) => {
                    const position: LatLngTuple = Array.isArray(marker.position) && marker.position.length === 2
