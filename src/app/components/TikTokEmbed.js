8b6b41bd4e25a574031e6dbd5c2700f02bff35b4
ju3tin <ju3tin@hotmail.co.uk>
2024-11-10 15:35:11 +0000

    wer

--- src/app/components/TikTokEmbed.js
+++ src/app/components/TikTokEmbed.js
@@ -1,7 +1,12 @@
-// components/TikTokEmbed.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const TikTokEmbed = ({ url }) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true); // Set to true after the component mounts
+  }, []);
+
   return (
     <blockquote
       className="tiktok-embed"
@@ -9,9 +14,16 @@ const TikTokEmbed = ({ url }) => {
       data-video-id={url.split('/').pop()}
       style={{ maxWidth: '605px', margin: '0 auto', backgroundColor: 'black' }}
     >
-      <section>
-        <a href={url}>Watch this TikTok video</a>
-      </section>
+      {isClient ? ( // Render iframe only on the client
+        <section>
+          <a href={url}>Watch this TikTok video</a>
+          <iframe src={url} title="TikTok Video" />
+        </section>
+      ) : (
+        <section>
+          <a href={url}>Watch this TikTok video</a>
+        </section>
+      )}
     </blockquote>
   );
 };
