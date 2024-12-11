36e904906a9dfe53a777e81c6095a9da87328080
ju3tin <ju3tin@hotmail.co.uk>
2024-12-11 16:21:53 +0000

    sdf

---
+++ src/app/about/page.tsx
@@ -0,0 +1,24 @@
+
+export default function Roadmap() {
+    return (
+<div id="container12" className="container default pt-4">
+    <div className="wrapper1">
+        <div className="inner">
+      <h1 className='text-3xl font-semibold capitalize'>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sapiente
+        tenetur animi excepturi tempora aperiam, fugit in architecto, iusto
+        magni placeat obcaecati voluptatem soluta labore similique reiciendis
+        non a harum autem id provident omnis corrupti consequuntur velit?
+        Architecto repellat maiores quam ducimus nobis veritatis delectus
+        molestias, inventore corrupti modi minima commodi quos fugit odit
+        ratione tempore iure ipsam soluta harum sequi quas excepturi! Tenetur
+        laboriosam laborum quibusdam enim dicta adipisci, nostrum ipsa provident
+        odio nemo dolore optio, ad doloremque ducimus praesentium iure neque
+        voluptas. Aliquid eveniet incidunt facere qui itaque reiciendis labore
+        est, facilis, iusto ex iste. Eos, ducimus iure!
+      </p>
+        </div>
+    </div>
+</div>
+)}
