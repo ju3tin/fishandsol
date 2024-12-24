85b935b9df6196c713ea80d99f8854ff8dca8406
ju3tin <ju3tin@hotmail.co.uk>
2024-12-24 12:02:37 +0000

    f

--- src/app/item/[item]/page.tsx
+++ src/app/item/[item]/page.tsx
@@ -1,20 +1,29 @@
-export default function ItemPage({ params }: { params: { item: string } }) {
-  return (
-    <div>
-      <h1 className='text-3xl font-semibold capitalize'>{params.item} Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sapiente
-        tenetur animi excepturi tempora aperiam, fugit in architecto, iusto
-        magni placeat obcaecati voluptatem soluta labore similique reiciendis
-        non a harum autem id provident omnis corrupti consequuntur velit?
-        Architecto repellat maiores quam ducimus nobis veritatis delectus
-        molestias, inventore corrupti modi minima commodi quos fugit odit
-        ratione tempore iure ipsam soluta harum sequi quas excepturi! Tenetur
-        laboriosam laborum quibusdam enim dicta adipisci, nostrum ipsa provident
-        odio nemo dolore optio, ad doloremque ducimus praesentium iure neque
-        voluptas. Aliquid eveniet incidunt facere qui itaque reiciendis labore
-        est, facilis, iusto ex iste. Eos, ducimus iure!
-      </p>
-    </div>
-  );
+import { GetServerSideProps, NextPage } from 'next';
+
+interface MyPageProps {
+    params: {
+        item: string;
+    };
 }
+
+const MyPage: NextPage<MyPageProps> = ({ params }) => {
+    return (
+        <div>
+            <h1>Item: {params.item}</h1>
+        </div>
+    );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { item } = context.params as { item: string }; // Type assertion
+
+    return {
+        props: {
+            params: {
+                item,
+            },
+        },
+    };
+};
+
+export default MyPage;
