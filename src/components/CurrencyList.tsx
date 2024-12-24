bd97fcf6ff3b9e79fca9b9eb46ce637bcfc3b239
ju3tin <ju3tin@hotmail.co.uk>
2024-12-24 14:14:52 +0000

    f

--- src/components/CurrencyList.tsx
+++ src/components/CurrencyList.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image';
-
+import Image from "next/image";
 import {
 	Select,
 	SelectContent,
@@ -10,27 +9,31 @@ import {
 	SelectValue,
 } from "@/components/ui/select";
 
-import { currencies } from '../lib/currencies';
-
-import styles from '../styles/components/CurrencyList.module.css';
+import { currencies } from "../lib/currencies";
+import styles from "../styles/components/CurrencyList.module.css";
 
 export type CurrencyListProps = {
-	balances: Record<string, string>
-}
+	balances: Record<string, string>; // Currency balances
+	onCurrencyChange: (selectedCurrency: string) => void; // Callback for selected currency
+};
 
 export default function CurrencyList({
 	balances,
-	...props
+	onCurrencyChange,
 }: CurrencyListProps) {
+	const handleChange = (value: string) => {
+		onCurrencyChange(value); // Notify parent component
+	};
+
 	return (
-		<Select defaultValue={currencies[0].id} {...props}>
+		<Select defaultValue={currencies[0].id} onValueChange={handleChange}>
 			<SelectTrigger>
 				<SelectValue placeholder="Select a currency" />
 			</SelectTrigger>
 			<SelectContent>
 				<SelectGroup>
 					<SelectLabel>Currencies</SelectLabel>
-					{currencies.map((currency) =>
+					{currencies.map((currency) => (
 						<SelectItem
 							value={currency.id}
 							className={styles.SelectItem}
@@ -38,16 +41,15 @@ export default function CurrencyList({
 						>
 							<div className={styles.CurrencyItem}>
 								<div className={styles.CurrencyLabel}>
-									<Image src={currency.icon} alt={currency.units} />
+									<Image src={currency.icon} alt={currency.units} width={20} height={20} />
 									<span>{currency.name}</span>
 								</div>
 								<div>
-									{balances[currency.id] ?? '-'}{" "}
-									{currency.units}
+									{balances[currency.id] ?? "-"} {currency.units}
 								</div>
 							</div>
 						</SelectItem>
-					)}
+					))}
 				</SelectGroup>
 			</SelectContent>
 		</Select>
