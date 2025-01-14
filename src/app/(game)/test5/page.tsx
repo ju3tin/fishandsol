d92082d1008a0917d15e11c4f31f802a14a04eb5
ju3tin <ju3tin@hotmail.co.uk>
2025-01-14 19:11:29 +0000

    sa

--- src/app/(game)/test5/page.tsx
+++ src/app/(game)/test5/page.tsx
@@ -5,7 +5,7 @@ import CrashList from '../../../components/CrashList1';
 import GameControls from '../../../components/GameControls1';
 import BetList from '../../../components/BetList1';
 
-import GameLayout from '../../../components/GameLayout';
+import GameLayout from '../../../components/GameLayout1';
 
 export default function Home() {
 	return (
