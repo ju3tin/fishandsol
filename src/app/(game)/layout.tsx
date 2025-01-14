b01d65a77a561198d90522cebf2df8f9ddc7ba75
ju3tin <ju3tin@hotmail.co.uk>
2025-01-14 21:27:46 +0000

    sd

--- src/app/(game)/layout.tsx
+++ src/app/(game)/layout.tsx
@@ -4,6 +4,8 @@ import { Toaster } from "@/components/ui/sonner"
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Sidebar } from '@/components/sidebar';
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -24,10 +26,11 @@ export default function RootLayout({
 	return (
 			<html lang="en">
 				<body className={inter.className}>
-					<PageHeader />
+        <Sidebar />
+				
 					{children}
 					<Toaster />
-					<PageFooter />
+				
 				</body>
 			</html>
 	);
