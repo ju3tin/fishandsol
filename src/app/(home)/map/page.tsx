d8ea1411737e1da74440f372c980afc55c2be4ea
ju3tin <ju3tin@hotmail.co.uk>
2025-01-14 08:55:38 +0000

    sddd

--- src/app/(home)/map/page.tsx
+++ src/app/(home)/map/page.tsx
@@ -3,7 +3,7 @@ import dynamic from "next/dynamic";
 import { useMemo } from "react";
 
 const Map = dynamic(
-    () => import('@/components/map/'),
+    () => import('../../../components/map/'),
     {
         loading: () => <p>A map is loading</p>,
         ssr: false
