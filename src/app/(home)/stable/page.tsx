0ac5e801ecfe68f85a70394adff799b29636ca56
ju3tin <ju3tin@hotmail.co.uk>
2025-01-15 14:00:09 +0000

    sd

--- src/app/(home)/stable/page.tsx
+++ src/app/(home)/stable/page.tsx
@@ -82,6 +82,7 @@ function App() {
       <h2 className="headline">
         <span className="headline-gradient">Chippy Ⓜ</span> (Solana)
       </h2>
+      <iframe src="https://widget.coinmarketcap.com/v2/ticker/coin-id/?lang=en" width="500" height="500"></iframe>
       <section className="customer-section px-10 pt-5 pb-10">
         {error && <p className="text-2xl text-red-700">{error}</p>}
         <div className="mt-5">
