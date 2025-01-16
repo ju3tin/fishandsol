2ed2ec3168764b6931a38e10fbe20c9dd6816a89
ju3tin <ju3tin@hotmail.co.uk>
2025-01-16 00:55:36 +0000

    ass

--- src/app/(home)/404/page.tsx
+++ src/app/(home)/404/page.tsx
@@ -3,13 +3,11 @@ import { Link } from 'react-router-dom'; // Or 'next/link' if you're using Next.
 
 export default function NotFoundPage() {
     return (
-<div id="container12" className="container default pt-4">
-    <div className="wrapper1">
-        <div className="inner">
-     Error 404
+<div className="container default pt-4">
+  
+    Error 404
 
      
-     </div>
-    </div>
+   
 </div>
 )}
