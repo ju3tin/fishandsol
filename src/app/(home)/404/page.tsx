636eeda91f0b2c13c52dbaf7c4736def07d4a5d2
ju3tin <ju3tin@hotmail.co.uk>
2025-01-16 00:47:34 +0000

    sdd

--- src/app/(home)/404/page.tsx
+++ src/app/(home)/404/page.tsx
@@ -6,15 +6,9 @@ export default function NotFoundPage() {
 <div id="container12" className="container default pt-4">
     <div className="wrapper1">
         <div className="inner">
-        <h1>404</h1>
-                    <h2>Page Not Found</h2>
-                    <p>
-                        Oops! The page you are looking for does not exist or has been moved.
-                    </p>
+     Error 404
 
-     <Link to="/">
-                        Go back to Home
-                    </Link>
+     
      </div>
     </div>
 </div>
