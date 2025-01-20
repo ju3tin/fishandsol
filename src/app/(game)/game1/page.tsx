7a081be6b2df6339e91345f983cddc36693bf388
ju3tin <ju3tin@hotmail.co.uk>
2025-01-20 13:22:55 +0000

    df

--- src/app/(game)/game1/page.tsx
+++ src/app/(game)/game1/page.tsx
@@ -6,28 +6,16 @@ import GameControls from '../../../components/GameControls2';
 import BetList from '../../../components/BetList2';
 
 import GameLayout from '../../../components/GameLayout2';
-import { WalletProvider } from '@solana/wallet-adapter-react';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { ConnectionProvider } from '@solana/wallet-adapter-react';
-import { clusterApiUrl } from '@solana/web3.js';
-
 
 export default function Home() {
-	const endpoint = clusterApiUrl('devnet');
-	const wallets = [new PhantomWalletAdapter()];
-
 	return (
 		<main className={styles.main}>
-			<ConnectionProvider endpoint={endpoint}>
-			<WalletProvider wallets={wallets}>
 			<GameLayout>
 			<CrashList />
 				<Game />
 			<GameControls />
 				<BetList />
-				</GameLayout>
-				</WalletProvider>
-				</ConnectionProvider>
+			</GameLayout>
 		</main>
 	);
 }
