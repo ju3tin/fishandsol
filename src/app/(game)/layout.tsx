c7a0255eb6a5d561b1529be053f382c6237a2d80
ju3tin <ju3tin@hotmail.co.uk>
2025-01-20 13:17:42 +0000

    fgh

--- src/app/(game)/layout.tsx
+++ src/app/(game)/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 //import RootProvider from '../providers/RootProvider';
 
 import { Toaster } from "@/components/ui/sonner"
@@ -8,12 +10,22 @@ import { Sidebar } from '@/components/sidebar';
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
+import { WalletProvider } from '@solana/wallet-adapter-react';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { ConnectionProvider } from '@solana/wallet-adapter-react';
+import { clusterApiUrl } from '@solana/web3.js';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+require('@solana/wallet-adapter-react-ui/styles.css');
+
 const inter = Inter({ subsets: ["latin"] });
 
 import PageHeader from '../components/PageHeader';
 import PageFooter from '../components/PageFooter';
 
-export const metadata: Metadata = {
+const wallets = [new PhantomWalletAdapter()];
+const endpoint = clusterApiUrl('devnet');
+
+const metadata: Metadata = {
 	title: "Crash",
 	description: "Crash",
 };
@@ -26,11 +38,17 @@ export default function RootLayout({
 	return (
     <html lang='en' className='dark'>
 				<body className={inter.className}>
-        <Sidebar />
+        <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>
+              <Sidebar />
 				
-        <main className='mx-5 mt-16 sm:ml-[300px] sm:mt-3'>{children}
-					<Toaster /></main>
+              <main className='mx-5 mt-16 sm:ml-[300px] sm:mt-3'>{children}
+                <Toaster /></main>
 				
+            </WalletModalProvider>
+          </WalletProvider>
+        </ConnectionProvider>
 				</body>
 			</html>
 	);
