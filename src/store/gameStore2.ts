1c7b95e99ac3590138fd91c9580bdb800e8024ca
ju3tin <ju3tin@hotmail.co.uk>
2025-01-20 21:20:19 +0000

    sss

--- src/store/gameStore2.ts
+++ src/store/gameStore2.ts
@@ -368,7 +368,7 @@ export const useGameStore = create<GameState>((set, get) => {
 			break;
 		case 'BET_MADE':
 			//roundStartTimestamp = new Date(); // Store the current timestamp globally
-			console.log(`Bet made by, ${message1[0]} ${message1[1]} ${message1.player}, ${message1.bet} ${message1.currency}`); // Log the timestamp
+			console.log(`Bet made by, ${message1.player}, ${message1.bet} ${message1.currency}`); // Log the timestamp
 		break;
 		  default:
 			console.log(`Unknown action received: ${message1.action}`);
