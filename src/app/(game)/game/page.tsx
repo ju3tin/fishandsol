11e563797a48c422c0af05b0fa180d849082a2d2
ju3tin <ju3tin@hotmail.co.uk>
2025-01-22 10:46:26 +0000

    sd

--- src/app/(game)/game/page.tsx
+++ src/app/(game)/game/page.tsx
@@ -1,9 +1,11 @@
 import styles from "./page.module.css";
+import axios from "axios";
 
 import Game from '../../../components/Game2';
 import CrashList from '../../../components/CrashList2';
 import GameControls from '../../../components/GameControls2';
 import BetList from '../../../components/BetList2';
+import axiosInstance from '../../../../lib/axiosInstance';
 
 import GameLayout from '../../../components/GameLayout2';
 
