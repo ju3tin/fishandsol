cc22f7c07a6c11e1e3dfebe00df680cff47623ab
ju3tin <ju3tin@hotmail.co.uk>
2025-01-29 15:52:52 +0000

    sssppp

--- src/components/Game2.tsx
+++ src/components/Game2.tsx
@@ -227,7 +227,7 @@ function drawCrashedRocket(
 	y: number,
 ) {
 	context.translate(x - rocketWidth/2, y - rocketWidth/2);
-	context.drawImage(explodeImage, 2000, -1100, 600, 600);
+	context.drawImage(explodeImage, 2000, -1100, 800, 800);
 	const text = `Launch in  secs`;
 	
 }
