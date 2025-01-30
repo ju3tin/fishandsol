48ffea64d04d8f133a40ef8eb688bac56ba5ca35
ju3tin <ju3tin@hotmail.co.uk>
2025-01-30 02:22:56 +0000

    update

--- src/app/(home)/404/page.tsx
+++ src/app/(home)/404/page.tsx
@@ -1,18 +1,8 @@
-"use client"; // Ensure this is a client component
 
-import { useSession } from 'next-auth/react';
-
-export default function NotFoundPage() {
-  const { data: session } = useSession();
-
-  return (
+export default function NotFound() {
+    return (
     <div className="container default pt-4">
       <h1>Error 404</h1>
-      {session?.user ? (
-        <p>Welcome back, {session.user.name}!</p>
-      ) : (
-        <p>Page not found. Please log in.</p>
-      )}
+     
     </div>
-  );
-}
+ )}
