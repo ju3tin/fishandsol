e6433f0de427e48c1e1897951febb455d57e8089
ju3tin <ju3tin@hotmail.co.uk>
2025-01-30 00:30:39 +0000

    sddd

---
+++ src/app/(game)/_app.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { SessionProvider } from 'next-auth/react';
+import { AppProps } from 'next/app';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+} 
--- src/app/(game)/login/page.tsx
+++ src/app/(game)/login/page.tsx
@@ -3,22 +3,17 @@
 "use client";
 import { signIn, signOut, useSession } from 'next-auth/react';
 
+
 export default function Home() {
   const { data: session } = useSession();
 
   return (
     <div>
       <h1>Welcome to My Next.js App</h1>
-      {session ? (
+      {session?.user ? (
         <>
-          {session?.user ? (
-            <>
-              <p>Signed in as {session.user.name}</p>
-              <button onClick={() => signOut()}>Sign out</button>
-            </>
-          ) : (
-            <p>Not signed in</p>
-          )}
+          <p>Signed in as {session.user.name}</p>
+          <button onClick={() => signOut()}>Sign out</button>
         </>
       ) : (
         <>
