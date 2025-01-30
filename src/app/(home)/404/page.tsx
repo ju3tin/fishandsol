14bf166ccc7173a6285362c30fc3893b7a628598
ju3tin <ju3tin@hotmail.co.uk>
2025-01-30 02:18:05 +0000

    asd

--- src/app/(home)/404/page.tsx
+++ src/app/(home)/404/page.tsx
@@ -8,7 +8,11 @@ export default function NotFoundPage() {
   return (
     <div className="container default pt-4">
       <h1>Error 404</h1>
-     
+      {session?.user ? (
+        <p>Welcome back, {session.user.name}!</p>
+      ) : (
+        <p>Page not found. Please log in.</p>
+      )}
     </div>
   );
 }
