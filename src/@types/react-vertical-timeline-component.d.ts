6516fd43e49935377fd30bd0c3058ceaab13ca73
ju3tin <ju3tin@hotmail.co.uk>
2025-01-30 09:33:33 +0000

    asd

---
+++ src/@types/react-vertical-timeline-component.d.ts
@@ -0,0 +1,6 @@
+// src/@types/react-vertical-timeline-component.d.ts
+declare module 'react-vertical-timeline-component' {
+    export const VerticalTimeline: any;
+    export const VerticalTimelineElement: any;
+    // Add other exports as needed
+  }
