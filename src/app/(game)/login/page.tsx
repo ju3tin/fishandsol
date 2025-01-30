15e5e350194dce3647c02552e6bac4a94e98e915
ju3tin <ju3tin@hotmail.co.uk>
2025-01-30 00:22:40 +0000

    sd

---
+++ src/app/(game)/login/page.tsx
@@ -0,0 +1,29 @@
+// pages/index.js
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+export default function Home() {
+  const { data: session } = useSession();
+
+  return (
+    <div>
+      <h1>Welcome to My Next.js App</h1>
+      {session ? (
+        <>
+          {session?.user ? (
+            <>
+              <p>Signed in as {session.user.name}</p>
+              <button onClick={() => signOut()}>Sign out</button>
+            </>
+          ) : (
+            <p>Not signed in</p>
+          )}
+        </>
+      ) : (
+        <>
+          <p>Not signed in</p>
+          <button onClick={() => signIn('twitter')}>Sign in with Twitter</button>
+        </>
+      )}
+    </div>
+  );
+}
