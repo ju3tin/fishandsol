66ccb9b08172985efdc35c7975519e863368ac62
ju3tin <ju3tin@hotmail.co.uk>
2025-01-30 23:27:25 +0000

    wewww

--- src/providers/WalletContextProvider.tsx
+++ src/providers/WalletContextProvider.tsx
@@ -9,7 +9,7 @@ export default function App({ children }: { children: React.ReactNode }) {
   const network = WalletAdapterNetwork.Devnet;
 
   // Configure the network connection
-  const endpoint = useMemo(() => clusterApiUrl(network), []);
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
   // Configure wallets
   const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
