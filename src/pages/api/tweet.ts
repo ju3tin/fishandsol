56f5cf70b7ba533f8aea58642528fdf581aaa4ea
ju3tin <ju3tin@hotmail.co.uk>
2025-02-07 12:34:05 +0000

    sdf

--- src/pages/api/tweet.ts
+++ src/pages/api/tweet.ts
@@ -5,7 +5,7 @@ import fs from "fs";
 import path from "path";
 import { promisify } from "util";
 import * as dotenv from "dotenv";
-import { Request } from "express";
+import { Request, Response } from "express";
 
 dotenv.config();
 
@@ -29,7 +29,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method !== "POST") return res.status(405).json({ error: "Method Not Allowed" });
 
   try {
-    await uploadMiddleware(req as Request, res);
+    await uploadMiddleware(req as unknown as Request, res as unknown as Response);
 
     const { tweetText } = req.body;
     if (!tweetText) return res.status(400).json({ error: "Tweet text is required" });
