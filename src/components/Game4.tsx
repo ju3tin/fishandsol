4960f3ba70a4f19e2e8f17a5986dd1a7979e7db6
ju3tin <ju3tin@hotmail.co.uk>
2025-02-10 11:19:19 +0000

    sdf

--- src/components/Game4.tsx
+++ src/components/Game4.tsx
@@ -4,7 +4,7 @@ import { useRef, useEffect } from "react";
 import * as THREE from "three";
 import { useGameStore, GameState } from "../store/gameStore2";
 import styles from "../styles/Game1.module.css";
-import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+//import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 
 const height = 2000;
 const coeffB = 0.5;
@@ -21,7 +21,7 @@ export default function Game() {
     useEffect(() => {
         const loadFBXModel = async () => {
             // ✅ Dynamic import to prevent build errors
-         //   const { FBXLoader } = await import("three/examples/jsm/loaders/FBXLoader");
+            const { FBXLoader } = await import("three/examples/jsm/loaders/FBXLoader");
             const threeCanvas = threeCanvasRef.current;
             if (!threeCanvas) return;
 
