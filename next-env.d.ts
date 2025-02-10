6447e5d6b9a3a9c44f69386b88ad41ca16503ef1
ju3tin <ju3tin@hotmail.co.uk>
2025-02-10 10:48:07 +0000

    sdf

---
+++ next-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+/// <reference types="next/navigation-types/compat/navigation" />
+declare module "three/examples/jsm/loaders/FBXLoader";
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
