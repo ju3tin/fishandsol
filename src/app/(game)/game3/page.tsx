51534e0ab3e45e11287dc7fc3934d155d64f0fc3
ju3tin <ju3tin@hotmail.co.uk>
2025-02-10 12:12:15 +0000

    sdf

--- src/app/(game)/game3/page.tsx
+++ src/app/(game)/game3/page.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { Suspense } from "react"
 import { Canvas, useLoader } from "@react-three/fiber"
 import { Environment, OrbitControls } from "@react-three/drei"
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
 
 const Model = () => {
   // location of the 3D model
