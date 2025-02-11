9b74eeba95e636cdde1ecca597d4ac5da81a53c6
ju3tin <ju3tin@hotmail.co.uk>
2025-02-11 08:22:51 +0000

    sdf

--- src/components/Game5.tsx
+++ src/components/Game5.tsx
@@ -14,7 +14,7 @@ export default function ThreeScene() {
   useEffect(() => {
     // ✅ Create a Three.js scene
     const scene = new THREE.Scene();
-    scene.add(new THREE.AxesHelper(5));
+    scene.add(/*new THREE.AxesHelper(5)*/);
 
     const light = new THREE.PointLight(0xffffff, 50);
     light.position.set(0.8, 1.4, 1.0);
