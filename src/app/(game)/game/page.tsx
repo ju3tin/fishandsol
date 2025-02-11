6d76d1798229dbcf20e3342771bda61dfb719783
ju3tin <ju3tin@hotmail.co.uk>
2025-02-11 17:48:31 +0000

    update

--- src/app/(game)/game/page.tsx
+++ src/app/(game)/game/page.tsx
@@ -1,7 +1,10 @@
+'use client';
 import styles from "./page.module.css";
 import axios from "axios";
+import { useRef, useEffect, useState, useCallback } from 'react';
 
 import Game from '../../../components/Game2';
+import Game5 from '../../../components/Game6';
 import CrashList from '../../../components/CrashList2';
 import GameControls from '../../../components/GameControls2';
 import BetList from '../../../components/BetList2';
@@ -13,14 +16,33 @@ import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
 
 
 export default function Home() {
+	const [layoutWidth, setLayoutWidth] = useState(0);
+
+	useEffect(() => {
+		const handleResize = () => {
+		  setLayoutWidth(window.innerWidth); // Get width dynamically
+		};
+	
+		// Initial load
+		handleResize();
+	
+		// Listen for resize events
+		window.addEventListener('resize', handleResize);
+	
+		return () => {
+		  window.removeEventListener('resize', handleResize); // Cleanup
+		};
+	  }, []);
+	
+
 	return (
 		<main className={styles.main1}>
-			<GameLayout>
-			<CrashList />
-				<Game />
-			<GameControls />
-				<BetList />
-			</GameLayout>
-		</main>
+		<GameLayout width={layoutWidth}> {/* Pass the layoutWidth as width prop */}
+		  <CrashList />
+		  <Game />
+	  <GameControls />
+		  <BetList />
+		</GameLayout>
+	  </main>
 	);
 }
