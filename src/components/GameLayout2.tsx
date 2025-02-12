1f00dc2bb3f60648efe75af75819f79d16f6cabb
ju3tin <ju3tin@hotmail.co.uk>
2025-02-12 12:24:50 +0000

    dfg

--- src/components/GameLayout2.tsx
+++ src/components/GameLayout2.tsx
@@ -1,11 +1,15 @@
-import { PropsWithChildren }from 'react';
+import { PropsWithChildren } from 'react';
+import React from 'react';
 
 import styles from '../styles/GameLayout1.module.css';
 
-export type GameLayoutProps = PropsWithChildren;
-
-export default function GameLayout({
-	children
-}: GameLayoutProps) {
-	return <div className={styles.GameLayout}>{children}</div>;
+interface GameLayoutProps {
+	children: React.ReactNode;
+	width: number;
 }
+
+const GameLayout: React.FC<GameLayoutProps> = ({ children, width }) => {
+	return <div style={{ width: `${width}px` }} className={styles.GameLayout}>{children}</div>;
+};
+
+export default GameLayout;
