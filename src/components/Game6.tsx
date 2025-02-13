98ac9ad6003d8bcd534eba059dcf4d6d2ee90e11
ju3tin <ju3tin@hotmail.co.uk>
2025-02-13 15:11:03 +0000

    sdf

--- src/components/Game6.tsx
+++ src/components/Game6.tsx
@@ -67,7 +67,7 @@ export default function ThreeScene({ width }: ThreeSceneProps) {
     const fontLoader = new FontLoader();
     fontLoader.load('/examples/fonts/helvetiker_regular.typeface.json', (font) => {
       fontRef.current = font;
-      if (gameState.status === 'Waiting') {
+      if (gameState.status == 'Waiting') {
         updateText(gameState.timeRemaining.toString());
       }else{
         updateText(gameState.status)
