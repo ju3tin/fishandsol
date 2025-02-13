02325aae77315a3b10c266f4b0f7586df447336a
ju3tin <ju3tin@hotmail.co.uk>
2025-02-13 21:45:26 +0000

    sdf

--- src/components/Game6.tsx
+++ src/components/Game6.tsx
@@ -66,7 +66,7 @@ export default function ThreeScene({ width }: ThreeSceneProps) {
 
       if (object.animations.length > 0) {
         const action = mixer.clipAction(object.animations[0]); // Play the first animation
-    action.setEffectiveTimeScale(0.5); // Slows animation to 50% speed
+    action.setEffectiveTimeScale(0.25); // Slows animation to 50% speed
     action.play();
       }
 
