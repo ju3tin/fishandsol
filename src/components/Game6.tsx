e7adfe5bcc836031ef06b4422a54110668fceedf
ju3tin <ju3tin@hotmail.co.uk>
2025-02-15 22:58:32 +0000

    update

--- src/components/Game6.tsx
+++ src/components/Game6.tsx
@@ -145,8 +145,10 @@ export default function ThreeScene({ width }: ThreeSceneProps) {
     if (!isNaN(state.timeRemaining)) {
     const progress = (10 - state.timeRemaining) / 10; // Normalize to range 0 - 1
     fishRef.current.position.z = THREE.MathUtils.lerp(initialPosition, finalPosition, progress);
-   //fishRef.current.translateZ = (THREE.MathUtils.lerp(initialPosition, finalPosition, progress)); // Move fish closer to the camera
-    }
+    //fishRef.current.translateZ(THREE.MathUtils.lerp(initialPosition, finalPosition, progress)); // Move fish closer to the camera 
+    //const distance = THREE.MathUtils.lerp(initialPosition, finalPosition, progress); // Calculate the distance
+    //fishRef.current.translateZ(distance); // Move fish smoothly closer to the camera
+  }
   }
   if (fishRef.current && state.status === 'Running'){
 
