caac47c96a5ef730095a3fe26292b5270c93c002
ju3tin <ju3tin@hotmail.co.uk>
2025-03-12 08:46:06 +0000

    update

--- src/app/(update1)/profile/page.tsx
+++ src/app/(update1)/profile/page.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+'use client';
+import styles from "./page.module.css";
+import axios from "axios";
+import { useRef, useEffect, useState, useCallback } from 'react';
 
-import PagePlaceholder from '@/components/page-placeholder';
+import Game from '../../../components/Game2';
+import Game5 from '../../../components/Game6';
+import CrashList from '../../../components/CrashList2';
+import GameControls from '../../../components/GameControls2';
+import BetList from '../../../components/BetList2';
+import axiosInstance from '../../../../lib/axiosInstance';
 
-export default function ProfilePage() {
-  return <PagePlaceholder pageName="Profile" />;
+import GameLayout from '../../../components/GameLayout3';
+import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogClose } from '@radix-ui/react-dialog';
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
+
+
+export default function Home() {
+	const [layoutWidth, setLayoutWidth] = useState(0);
+
+	useEffect(() => {
+		const handleResize = () => {
+		  setLayoutWidth(window.innerWidth); // Get width dynamically
+		};
+	
+		// Initial load
+		handleResize();
+	
+		// Listen for resize events
+		window.addEventListener('resize', handleResize);
+	
+		return () => {
+		  window.removeEventListener('resize', handleResize); // Cleanup
+		};
+	  }, []);
+	
+
+	return (
+		<main className={styles.main1}>
+		<GameLayout> {/* Pass the layoutWidth as width prop */}
+		  <CrashList />
+		  <Game />
+	  <GameControls />
+		  <BetList />
+		</GameLayout>
+	  </main>
+	);
 }
