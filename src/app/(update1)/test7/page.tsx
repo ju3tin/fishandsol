24ed2f263bf62f1cf0b7d681677af213f45d480c
ju3tin <ju3tin@hotmail.co.uk>
2025-03-12 15:26:50 +0000

    update

---
+++ src/app/(update1)/test7/page.tsx
@@ -0,0 +1,48 @@
+"use client"
+import styles from "./page.module.css";
+
+import { useEffect, useState, useRef } from 'react';
+import Game from '../../../components/newGame5old';
+import CrashList from '../../../components/CrashList';
+import GameControls from '../../../components/GameControls';
+import BetList from '../../../components/BetList';
+import JsonFetcher from "../../components/JsonFetcher";
+import Chatroom from '../../../components/Chatroom1';
+import GameLayout from '../../../components/GameLayout';
+
+  
+export default function Home() {
+
+const [chatMessages, setChatMessages] = useState<Array<{ text: string; timestamp: string }>>([]);
+const [inputMessage, setInputMessage] = useState('');
+
+const ChatMessage = ({ timestamp, text }: { timestamp: string; text: string }) => (
+  <div className="chat-message">
+	<span className="timestamp">{timestamp}</span>
+	<span className="message">{text}</span>
+  </div>
+);
+
+	const [ws, setWs] = useState<WebSocket | null>(null);
+ 
+	const handleSendMessage = () => {
+		if (ws && inputMessage.trim()) {
+		  ws.send(inputMessage);
+		  setInputMessage('');
+		}
+	  };
+	return (
+		<main className={styles.main}>
+	 <Chatroom />
+				<GameLayout> {/* Pass the layoutWidth as width prop */}
+		 
+		
+	  <CrashList />
+	  <Game />
+	  <GameControls />
+
+		  <BetList />
+		</GameLayout>
+		</main>
+	);
+}
