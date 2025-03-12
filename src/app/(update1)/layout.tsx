45a1b6f9f447bacad61a966dbad7f85581f7acce
ju3tin <ju3tin@hotmail.co.uk>
2025-03-12 08:36:55 +0000

    update

--- src/app/(update1)/layout.tsx
+++ src/app/(update1)/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
             <SideNav />
             <div className="w-full overflow-x-auto">
               <div className="sm:h-[calc(99vh-60px)] overflow-auto ">
-                <div className="w-full flex justify-center mx-auto   overflow-auto h-[calc(100vh - 120px)] overflow-y-auto relative">
-                  <div className="w-full md:max-w-6xl">{children}</div>
+                <div className="">
+                  <div className="">{children}</div>
                 </div>
               </div>
             </div>
