8d6dbd4e33b65fe6e11066d7c3912a6c6e804c6b
ju3tin <ju3tin@hotmail.co.uk>
2025-03-26 17:04:14 +0000

    sdf

--- src/components/Chatroom1.tsx
+++ src/components/Chatroom1.tsx
@@ -26,7 +26,7 @@ const ChatMessage = ({ timestamp, text }: { timestamp: string; text: string }) =
 	  };
 	return (
 	
-<div className="chat-container p-4 bg-gray-900 text-white rounded-lg shadow-md" style={{ maxHeight: '500px' }}>
+<div className="chat-container p-4 text-white rounded-lg shadow-md" style={{ maxHeight: '500px' }}>
 <div style={{overflow:'scroll' , overflowX:'hidden', height: '408px' }} className="chat-messages">
 <JsonFetcher url="/api" />
   {chatMessages.map((msg, index) => {
