65f9446e91bd2666f7ee38b6ceec2708fed9f1b1
ju3tin <ju3tin@hotmail.co.uk>
2025-04-01 17:41:06 +0100

    Hide theme toggle button by setting its display to none for improved UI management.

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -70,7 +70,7 @@ export function ThemeToggle({ isDropDown = false }: { isDropDown?: boolean }) {
   }
   return (
     <>
-      <div className="flex flex-row space-x-2 items-center rounded-full border p-1">
+      <div style={{display:'none'}} className="flex flex-row space-x-2 items-center rounded-full border p-1">
         <button
           className={cn(
             theme === 'light'
