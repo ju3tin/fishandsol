efebcea2aa5c2b1c998ab8ff50cbed65fbde0ff9
ju3tin <ju3tin@hotmail.co.uk>
2025-04-09 13:30:51 +0100

    Update import path for styles in pest2/page.tsx to ensure correct referencing and maintain consistency across components.

--- src/app/(update1)/pest2/page.tsx
+++ src/app/(update1)/pest2/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import styles from "./page.module.css";
+import styles from "../page.module.css";
 
 import { useEffect, useState, useRef } from 'react';
 import Game from '../../../components/newGame5old';
