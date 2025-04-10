b59f4c4ff091167277c0d58ab221b13360b51a43
ju3tin <ju3tin@hotmail.co.uk>
2025-04-10 18:09:45 +0100

    Enhance Betbutton1A component by adding a new card layout for demo and wallet options, improving user interaction with overlays and QR code functionality. Update global.css for responsive card display adjustments on mobile and larger screens.

--- src/components/betbutton1a.tsx
+++ src/components/betbutton1a.tsx
@@ -77,7 +77,8 @@ const Betbutton = ({
   const audioRef1 = useRef<HTMLAudioElement>(null);
   const textToCopy = "This is the text to copy!";
   const gameState5 = useGameStore((state: GameState) => state);
-
+  const [previousTimeRemaining, setPreviousTimeRemaining] = useState<number | null>(null);
+    
   const handleCopy = () => {
     navigator.clipboard
       .writeText(textToCopy)
@@ -89,7 +90,9 @@ const Betbutton = ({
         console.error("Failed to copy text: ", err);
       });
   };
-
+  const handleChangeDemoAmount = (amount: string) => {
+    setDemoAmount(amount);
+  }
   const toggleOverlay = () => {
     setOverlayVisible(!overlayVisible);
   };
@@ -124,6 +127,7 @@ const Betbutton = ({
   }, [gameState5, buttonPressCount, buttonClicked]);
 
   useEffect(() => {
+    
     if (gameState5.status === "Crashed") {
       const timer = setTimeout(() => {
         setButtonPressCount(0);
@@ -168,9 +172,10 @@ const Betbutton = ({
     dude56(currency);
     console.log(`this is the checked ${currency}`);
   };
-
+  const balances = useGameStore((game: GameState) => game.balances);
   return (
-    <div className="lg:col-span-2 bg-black border-black">
+    <div>
+    <div className="card lg:col-span-2 bg-black border-black">
       <audio ref={audioRef} src="/sounds/cheering.mp3" />
       <audio ref={audioRef1} src="/sounds/losing.mp3" />
 
@@ -275,6 +280,261 @@ const Betbutton = ({
         </Button>
       </div>
     </div>
+    <div className="card1 lg:col-span-2 bg-black border-black">
+          <audio ref={audioRef} src="/sounds/cheering.mp3" /> {/* Add your MP3 file path here */}
+          <audio ref={audioRef1} src="/sounds/losing.mp3" /> {/* Add your MP3 file path here */}
+        <Card className="bg-black border-black">
+        {overlayVisible && (
+		 <div className="overlay">
+		   <div className="message-board-container">
+			 <div className="message-form">
+			   
+			 <Tabs aria-label="Options">
+        <Tab key="Sol" title="Sol">
+		<Tabs aria-label="Options">
+        <Tab key="Chippydeposit" title="Deposit">
+		  <Card>
+	  
+<div style={{ height: "auto", margin: "0 auto", maxWidth: 64, width: "100%" }}>
+  <QRCode
+    size={256}
+    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+    value='fuck you'
+    viewBox={`0 0 256 256`}
+  />
+        <p>{textToCopy}</p>
+      <a href="#" onClick={(e) => { e.preventDefault(); handleCopy(); }}>
+        {copied ? "Copied!" : "Copy to Clipboard"}
+      </a>
+</div>
+
+          </Card>
+		  </Tab>
+		  <Tab key="ChippyWithdraw" title="Withdraw">
+		  <Card>
+		  Enter a wallet address on the Solana network. Your withdrawal will be processed INSANTLY.
+<div style={{ height: "auto", margin: "0 auto", maxWidth: 64, width: "100%" }}>
+ <Label>Amount</Label>
+ <input type="text" id="username" 
+        placeholder="SOL" />
+ <Label>Wallet Address for Withdrawal</Label>
+ <input type="text" id="username" 
+        placeholder="Wallet Address" />
+
+<Button>Withdrawal</Button>
+ 
+</div>
+          </Card>
+		  </Tab>
+		  </Tabs>
+        </Tab>
+        <Tab key="Chippy" title="Chippy">
+		<Tabs aria-label="Options">
+        <Tab key="Chippydeposit" title="Deposit">
+		  <Card>
+		  
+		  <div style={{ height: "auto", margin: "0 auto", maxWidth: 64, width: "100%" }}>
+  <QRCode
+    size={256}
+    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+    value='fuck you'
+    viewBox={`0 0 256 256`}
+  />
+        <p>{textToCopy}</p>
+      <a href="#" onClick={(e) => { e.preventDefault(); handleCopy(); }}>
+        {copied ? "Copied!" : "Copy to Clipboard"}
+      </a>
+</div>
+          </Card>
+		  </Tab>
+		  <Tab key="ChippyWithdraw" title="Withdraw">
+		  <Card>
+		  Enter a wallet address on the Solana network. Your withdrawal will be processed INSANTLY.
+<div style={{ height: "auto", margin: "0 auto", maxWidth: 64, width: "100%" }}>
+<Label>Amount</Label>
+ <input type="text" id="username" 
+        placeholder="SOL" />
+ <Label>Wallet Address for Withdrawal</Label>
+ <input type="text" id="username" 
+        placeholder="Wallet Address" />
+
+<Button>Withdrawal</Button>
+</div>
+          </Card>
+		  </Tab>
+		  </Tabs>
+        </Tab>
+        <Tab key="Demo" title="Demo">
+<Card>
+	<Label>Demo Amount {demoAmount}</Label>
+	</Card>          
+		  <Card>
+Use demo currency to play our games without any risk. If you run out of demo credits, you can reset your demo balance anytime by clicking the button below. Have fun and enjoy your experience!
+		  <Button
+		  onClick={() => handleChangeDemoAmount('100')}
+		  >
+		  <FaWallet className={styles.walletIcon} /> {/* Icon from FontAwesome */}
+			Reset Demo Balance		
+		  </Button>
+          </Card>
+        </Tab>
+      </Tabs>
+			 
+			 </div>
+ 
+			
+ 
+			 <button onClick={toggleOverlay} className="close-overlay-btn">
+			   Close
+			 </button>
+		   </div>
+		 </div>
+	   )}
+
+{demoamountgreaterthan0 && (
+		<div className="overlay">
+		<div className="message-board-container">
+		<div className="message-form">
+		<p>Demo amount must be greater than 0.</p>
+		</div>
+		<button onClick={red2} className="close-overlay-btn">
+			   Close
+			 </button>
+		</div>
+		</div>
+	)}
+
+
+	   {betgreaterthan0 && (
+		<div className="overlay">
+			 <div className="message-board-container">
+			 <div className="message-form">
+		<p>Bet amount must be greater than 0.</p>
+		</div>
+		<button onClick={red1} className="close-overlay-btn">
+			   Close
+			 </button>
+		</div>
+		</div>
+	   )}
+          
+            <div className="space-y-4">
+            <div className={styles.inputGroup}>
+					<Label>Demo Amount</Label>
+					<Input
+						placeholder="Demo amount"
+						type="number"
+						min="0"
+						value={demoAmount}
+						disabled
+					/>
+				</div>
+              <div>
+                <Label htmlFor="bet-amount" className="text-white">
+                  Bet Amount (SOL)
+                </Label>
+                <Input
+                  id="bet-amount"
+                  type="number"
+                  value={betAmount}
+                  onChange={(e) => setBetAmount(e.target.value)}
+                  disabled={gameState5.status !== "Waiting" || buttonClicked || buttonPressCount === 1}
+                  className="bg-gray-700 border-gray-600 text-white"
+                  min="0.01"
+                  step="0.01"
+                />
+              </div>
+  
+              <div>
+                <Label htmlFor="auto-cashout" className="text-white">
+                  Auto Cashout
+                </Label>
+                <Checkbox
+          disabled={gameState5.status !== "Waiting" || buttonClicked || buttonPressCount === 1}
+					isSelected={isAutoCashOutDisabled}
+					onChange={(e) => handleCheckboxChange(e.target.checked)}
+				>
+					Disable Auto Cashout
+				</Checkbox>
+                <Input
+                  id="auto-cashout"
+                  type="number"
+                  value={autoCashoutAt}
+                  onChange={(e) => setAutoCashoutAt(e.target.value)}
+                  disabled={gameState5.status !== "Waiting" || buttonClicked || buttonPressCount === 1}
+                  className="bg-gray-700 border-gray-600 text-white"
+                  min="1.01"
+                  step="0.01"
+                />
+              </div>
+              <Label>Currency</Label>
+				<CurrencyList
+          buttonPressCount={buttonPressCount}
+          buttonClicked={buttonClicked}
+          gameState={gameState}
+					balances={balances} 
+					onCurrencyChange={setCurrency}
+				/>
+              {gameState5.status === "Waiting" ? (
+                <Button 
+                  onClick={buttonPressCount > 0 ? undefined : handleButtonPress} 
+                  className={`w-full ${buttonPressCount > 0 ? 'bg-gray-600' : 'bg-green-600 hover:bg-green-700'}`}
+                  disabled={buttonPressCount > 0}
+                >
+                  {buttonPressCount > 0 ? (
+                    <p className="text-black">Bet Placed</p>
+                  ) : (
+                    typeof gametime === 'number' && !isNaN(gametime) ? (
+                      <p className="text-black">Place Bet {gametime}</p>
+                    ) : (
+                      <p className="text-black">Place Bet {previousTimeRemaining}</p>
+                    )
+                  )}
+                </Button>
+              ) : gameState5.status === "Running" ? (
+                
+                
+                <Button
+                  onClick={handleCashout}
+                  className="w-full bg-yellow-600 hover:bg-yellow-700"
+                  disabled={userCashedOut || buttonClicked || buttonPressCount == 0}
+                >
+                  Cash Out ({currentMultiplier}x)
+                </Button>
+              ) : (
+                <Button disabled className="w-full bg-red-600">
+                  Crashed ({currentMultiplier}x)
+                </Button>
+              )}
+            </div>
+            <div>
+				<Button  onClick={toggleOverlay} className={styles.BetButton}>
+				<FaWallet className={styles.walletIcon} /> {/* Icon from FontAwesome */}
+					Deposit Chippy</Button></div>
+            {/* Active players */}
+           {/*  {gameState5.status !== "Waiting" && (
+              <div className="mt-6">
+                <h3 className="text-sm font-medium text-gray-400 mb-2">Active Players</h3>
+                <div className="space-y-2">
+                  {cashouts.length === 0 && !userCashedOut && (
+                    <div className="text-xs text-gray-500">Waiting for players to cash out...</div>
+                  )}
+                  {cashouts.map((cashout) => (
+                    <div key={cashout.id} className="flex justify-between items-center text-xs">
+                      <span className="font-medium text-white">{cashout.id === "you" ? "You" : cashout.id}</span>
+                      <span className="text-yellow-400">
+                        {cashout.amount.toFixed(2)} SOL @ {cashout.multiplier.toFixed(2)}x
+                      </span>
+                    </div>
+                  ))}
+                </div>
+                
+              </div>
+            )} */}
+         
+        </Card>
+      </div>
+</div>
   );
 };
 
