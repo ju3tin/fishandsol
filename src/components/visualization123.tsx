2ec131411e347a5eb0a48b02ca63d011cfe1020d
ju3tin <ju3tin@hotmail.co.uk>
2025-05-06 12:03:22 +0100

    update

--- src/components/visualization123.tsx
+++ src/components/visualization123.tsx
@@ -256,13 +256,13 @@ function animate() {
           
 
           <span style={{ 
-            top: '25px', 
+            top: '0px', 
             left: '50%', 
             transform: 'translateX(-50%)', 
             display: 'block', 
             position: 'absolute',
           }}>
-            <Image width={250} height={250} src="/explode1.svg" alt="Explosion effect" />
+            <Image width={275} height={275} src="/explode1.svg" alt="Explosion effect" />
           </span>
 
 
