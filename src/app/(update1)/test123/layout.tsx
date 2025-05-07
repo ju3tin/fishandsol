114bf415c173ae6f7f0e57a8206c35f141e6d434
ju3tin <ju3tin@hotmail.co.uk>
2025-05-07 15:27:04 +0100

    update

--- src/app/(update1)/test123/layout.tsx
+++ src/app/(update1)/test123/layout.tsx
@@ -1,13 +1,12 @@
-// pages/_app.tsx
-import type { AppProps } from "next/app";
+// layout.tsx for App Router
+"use client";
+
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
   ConnectionProvider,
   WalletProvider
 } from "@solana/wallet-adapter-react";
-import {
-  WalletModalProvider
-} from "@solana/wallet-adapter-react-ui";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import {
   PhantomWalletAdapter,
   SolflareWalletAdapter
@@ -15,7 +14,7 @@ import {
 import "@solana/wallet-adapter-react-ui/styles.css";
 import { useMemo } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = "https://api.devnet.solana.com";
 
@@ -27,9 +26,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <Component {...pageProps} />
-        </WalletModalProvider>
+        <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   );
