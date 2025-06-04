3c17c54796148b17f107a52d7f7680dcea687a5c
ju3tin <ju3tin@hotmail.co.uk>
2025-06-04 14:31:06 +0100

    update

--- src/types/crashgame.tsx
+++ src/types/crashgame.tsx
@@ -2,7 +2,7 @@ import { Idl, Program, Provider } from "@coral-xyz/anchor";
 import { PublicKey } from "@solana/web3.js";
 
 // Import the IDL
-import idl from "../idl.json";
+import idl from "./idl.json";
 export const IDL = idl as unknown as Idl;
 
 // Export the Program type
