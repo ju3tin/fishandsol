ede0c7abcc9f72ebdef6daee6e14a8b27aced7c4
ju3tin <ju3tin@hotmail.co.uk>
2025-06-04 17:06:43 +0100

    asd

--- src/components/newGame4.tsx
+++ src/components/newGame4.tsx
@@ -16,7 +16,7 @@ const CrashGraph: React.FC = () => {
    
     const canvas = canvasRef.current;
     if (!canvas) return;
-    const ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext("2d"); 
     if (!ctx) return;
 
     canvas.width = 500; // Adjust canvas size
