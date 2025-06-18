d820b8002e0b7c3052653a986b7a2f13ff03a6ac
ju3tin <ju3tin@hotmail.co.uk>
2025-06-18 16:34:19 +0100

    sdf

--- src/pages/api/checktoken2.ts
+++ src/pages/api/checktoken2.ts
@@ -15,6 +15,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     method: 'getAssetsByOwner',
     params: {
       ownerAddress: wallet,
+      limit: 1000
     },
   });
 
