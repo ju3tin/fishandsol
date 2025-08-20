dd6086a3a80f158eef5f20219bfa777b21be4150
ju3tin <ju3tin@hotmail.co.uk>
2025-08-20 15:13:49 +0100

    sfsdddd

--- src/app/(update1)/chippyfriday/page.tsx
+++ src/app/(update1)/chippyfriday/page.tsx
@@ -24,7 +24,7 @@ export default function TweetPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-start p-6">
       <h1 className="text-2xl font-bold mb-4 text-center">
-        Get Your Free Chips When you buy Chippy Tokens <Link href=""> Click Here</Link>
+        Get Your Free Chips When you buy Chippy Tokens <Link href="https://chipsandsol.vercel.app/tweet"> Click Here On Friday</Link>
       </h1>
       <div className="space-y-6 w-full max-w-xl">
         {tweets.map((tweet) => (
